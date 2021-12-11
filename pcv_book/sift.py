1c3eba250b1f47259998e5dbaa57877f7f3759ac
qiao <ffff0f0f@163.com>
2021-12-11 17:17:01 +0100

    update

--- pcv_book/sift.py
+++ pcv_book/sift.py
@@ -6,6 +6,7 @@ from pylab import *
 
 def process_image(imagename,resultname,params="--edge-thresh 10 --peak-thresh 5"):
     """ Process an image and save the results in a file. """
+    """处理图像并将结果保存在文件中。"""
 
     if imagename[-3:] != 'pgm':
         # create a pgm file
@@ -16,24 +17,28 @@ def process_image(imagename,resultname,params="--edge-thresh 10 --peak-thresh 5"
     cmmd = str("sift "+imagename+" --output="+resultname+
                 " "+params)
     os.system(cmmd)
-    print 'processed', imagename, 'to', resultname
+    print('processed', imagename, 'to', resultname)
 
 
 def read_features_from_file(filename):
     """ Read feature properties and return in matrix form. """
-    
+    """读取特征属性并以矩阵形式返回。"""
+
     f = loadtxt(filename)
     return f[:,:4],f[:,4:] # feature locations, descriptors
 
 
 def write_features_to_file(filename,locs,desc):
     """ Save feature location and descriptor to file. """
+    """将特征位置和描述符保存到文件。"""
     savetxt(filename,hstack((locs,desc)))
     
 
 def plot_features(im,locs,circle=False):
     """ Show image with features. input: im (image as array), 
         locs (row, col, scale, orientation of each feature). """
+    """ 显示具有特征的图像。
+    输入：im（图像作为数组），locs（每个特征的行、列、尺度和方向角度）。 """
 
     def draw_circle(c,r):
         t = arange(0,1.01,.01)*2*pi
@@ -48,6 +53,18 @@ def plot_features(im,locs,circle=False):
     else:
         plot(locs[:,0],locs[:,1],'ob')
     axis('off')
+'''
+# 绘制SIFT特征位置图像
+import sift
+imname = 'empire.jpg'
+im1 = array(Image.open(imname).convert('L'))
+stift.process_image(imname,'empire.sift')
+
+figure()
+gray()
+sift.plot_features(im1,l1,circle=True)
+show()
+'''
 
 
 def match(desc1,desc2):
@@ -55,7 +72,10 @@ def match(desc1,desc2):
         select its match in the second image.
         input: desc1 (descriptors for the first image), 
         desc2 (same for second image). """
-    
+    """ 对于第一幅图像中的每个描述符，选取在第二张图片中的匹配。
+         输入：desc1（第一张图像的描述子）
+             desc2（第二张图像的描述子）。 """
+
     desc1 = array([d/linalg.norm(d) for d in desc1])
     desc2 = array([d/linalg.norm(d) for d in desc2])
     
@@ -63,14 +83,16 @@ def match(desc1,desc2):
     desc1_size = desc1.shape
     
     matchscores = zeros((desc1_size[0]),'int')
-    desc2t = desc2.T # precompute matrix transpose
+    desc2t = desc2.T # precompute matrix transpose # 预先计算矩阵转置
     for i in range(desc1_size[0]):
         dotprods = dot(desc1[i,:],desc2t) # vector of dot products
         dotprods = 0.9999*dotprods
         # inverse cosine and sort, return index for features in second image
+        # 反余弦和排序，返回第二张图像中特征的索引
         indx = argsort(arccos(dotprods))
         
         # check if nearest neighbor has angle less than dist_ratio times 2nd
+        # 检查最近邻的角度是否小于 dist_ratio 乘以 第二近邻的角度
         if arccos(dotprods)[indx[0]] < dist_ratio * arccos(dotprods)[indx[1]]:
             matchscores[i] = int(indx[0])
     
@@ -115,7 +137,9 @@ def plot_matches(im1,im2,locs1,locs2,matchscores,show_below=True):
 
 def match_twosided(desc1,desc2):
     """ Two-sided symmetric version of match(). """
-    
+    """ 双向对称版本的match() ，
+    用另外的方法从第二幅到第一幅特征匹配，最后仅保留满足两种匹配准则的对应
+    """
     matches_12 = match(desc1,desc2)
     matches_21 = match(desc2,desc1)
     
